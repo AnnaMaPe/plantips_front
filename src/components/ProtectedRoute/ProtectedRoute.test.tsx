ddfca64699bba7863aad7dd7d324094cbb0caf45
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-11 12:50:21 +0100

    Test ProtectedRoute component

--- src/components/ProtectedRoute/ProtectedRoute.test.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.test.tsx
@@ -1,5 +1,26 @@
-describe("Given", () => {
-  describe("When", () => {
-    test("Then", () => {});
+import { screen } from "@testing-library/react";
+import { useAppSelector } from "../../store/hooks";
+import { renderRouterWithProviders } from "../../test.utils/renderWithProviders";
+import { ProtectedRoute } from "./ProtectedRoute";
+
+jest.mock("../../store/hooks", () => ({
+  useAppSelector: jest.fn(),
+}));
+
+describe("Given a ProtectedRoute component", () => {
+  describe("When it is rendered and there is a valid token", () => {
+    test("Then it should show the react element that receives by props", () => {
+      const state = { user: { token: "ThisIsAToken" } };
+      (useAppSelector as jest.Mock).mockImplementation((selector) =>
+        selector(state)
+      );
+      const reactElement = <div>Home Page</div>;
+      const expectedText = "Home Page";
+
+      renderRouterWithProviders(<ProtectedRoute element={reactElement} />);
+      const expectedReactElement = screen.getByText(expectedText);
+
+      expect(expectedReactElement).toBeInTheDocument();
+    });
   });
 });
