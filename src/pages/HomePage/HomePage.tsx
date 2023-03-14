a1fd5c77d92171b5056030e24d3de28457951f4b
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-14 12:05:10 +0100

    Create HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+export const HomePage = (): JSX.Element => {
+  return (
+    <>
+      <h1>User’s tips</h1>
+      <span>
+        Check out the best tips from other users to make your plants grow!
+      </span>
+    </>
+  );
+};
