70ab7eb8ee692ad62fd970d0188801ac8b066bd5
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-14 16:18:28 +0100

    Implement navigation to HomePage once user is logged

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,14 @@
+import { Navigate } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { useAppSelector } from "../../store/hooks";
 import LoginPageStyled from "./LoginPageStyled";
 
 const LoginPage = (): JSX.Element => {
-  return (
+  const { isLogged } = useAppSelector((state) => state.user);
+
+  return isLogged ? (
+    <Navigate to={"/"} replace={true} />
+  ) : (
     <LoginPageStyled className="login">
       <img
         className="login__photo"
--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 import App from "../components/App/App";
 import { ProtectedRoute } from "../components/ProtectedRoute/ProtectedRoute";
+import { HomePage } from "../pages/HomePage/HomePage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import { endpoints } from "./endpoints";
 
@@ -9,7 +10,7 @@ const routes: RouteObject[] = [
     path: "/",
     element: <App />,
     children: [
-      { path: "/", element: <ProtectedRoute element={<App />} /> },
+      { path: "/", element: <ProtectedRoute element={<HomePage />} /> },
       { path: endpoints.login, element: <LoginPage /> },
     ],
   },
