5f784af40c89bf1243cbba065cb4e96aa8c4b1ae
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-14 17:24:23 +0100

    Implement TipsList in HomePage

--- src/components/TipsList/TipsList.tsx
+++ src/components/TipsList/TipsList.tsx
@@ -1,12 +1,18 @@
-import { TipsStructure } from "../../store/features/tips/types";
+import { useEffect } from "react";
+import useApi from "../../hooks/useApi/useApi";
+import { useAppSelector } from "../../store/hooks";
 import { TipCard } from "../Card/TipCard";
 import { TipsListStyled } from "./TipsListStyled";
 
-interface TipsListProps {
-  tips: TipsStructure;
-}
+export const TipsList = (): JSX.Element => {
+  const { loadAllTips } = useApi();
 
-export const TipsList = ({ tips }: TipsListProps): JSX.Element => {
+  useEffect(() => {
+    loadAllTips();
+  }, [loadAllTips]);
+
+  const tips = useAppSelector((state) => state.tips.tips);
+
   return (
     <TipsListStyled className="list">
       {tips.map((tip) => (
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,16 @@
+import { TipsList } from "../../components/TipsList/TipsList";
 import { HomePageStyled } from "./HomePageStyled";
 
 export const HomePage = (): JSX.Element => {
   return (
     <HomePageStyled className="home">
-      <h1 className="home__title">User’s tips</h1>
-      <span>
-        Check out the best tips from other users to make your plants grow!
-      </span>
+      <div className="home__text">
+        <h1 className="home__title">User’s tips</h1>
+        <span className="home__description">
+          Check out the best tips from other users to make your plants grow!
+        </span>
+      </div>
+      <TipsList />
     </HomePageStyled>
   );
 };
