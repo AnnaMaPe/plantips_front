e4bf22da7b659f7c9089553fc6c22b8ae9959188
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-15 20:32:11 +0100

    Test condition to show buttons in Card depending on logged user

--- src/components/TipCard/TipCard.test.tsx
+++ src/components/TipCard/TipCard.test.tsx
@@ -1,5 +1,6 @@
 import { screen } from "@testing-library/react";
 import { monstera } from "../../mocks/tipsMocks";
+import { userIsLoggedState } from "../../mocks/userPreloadedState";
 import { renderWithProviders } from "../../test.utils/renderWithProviders";
 import { TipCard } from "./TipCard";
 
@@ -12,6 +13,19 @@ describe("Given a TipCard component", () => {
       const card = screen.getByRole("heading", { name: expectedTitle });
 
       expect(card).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered with a Monstera that was shared by the logged user", () => {
+    test("Then it should show a a button with the aria-label 'edit'", () => {
+      const expectedAriaLabel = "delete";
+
+      renderWithProviders(<TipCard tip={monstera} />, {
+        user: userIsLoggedState,
+      });
+      const button = screen.getByLabelText(expectedAriaLabel);
+
+      expect(button).toBeInTheDocument();
     });
   });
 });
