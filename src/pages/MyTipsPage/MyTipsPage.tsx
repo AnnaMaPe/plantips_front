081c94a69f70c9a32badf8b76b74a644d6193c0b
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-15 16:49:06 +0100

    Create MyTipsPage

---
+++ src/pages/MyTipsPage/MyTipsPage.tsx
@@ -0,0 +1,12 @@
+export const MyTipsPage = (): JSX.Element => {
+  return (
+    <>
+      <div className="my-tips__text">
+        <h1 className="my-tips__title">My tips</h1>
+        <span className="my-tips__description">
+          Check and modify the tips you have shared with our leafy community
+        </span>
+      </div>
+    </>
+  );
+};
