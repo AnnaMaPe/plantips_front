b8e64f5f36661c74629c643bebf9c93ecd2292c1
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-16 10:23:11 +0100

    Modify Button component to pass icon and a function by Props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,22 @@
+import { PropsWithChildren } from "react";
 import { ButtonStyled } from "./ButtonStyled";
 
 interface ButtonProps {
-  text: string;
-  isDisabled: boolean;
+  text?: string;
+  isDisabled?: boolean;
+  children?: PropsWithChildren;
+  action?: () => void;
 }
 
-export const Button = ({ text, isDisabled }: ButtonProps): JSX.Element => {
-  return <ButtonStyled disabled={isDisabled}>{text}</ButtonStyled>;
+export const Button = ({
+  text,
+  isDisabled,
+  action,
+  children,
+}: ButtonProps): JSX.Element => {
+  return (
+    <ButtonStyled onClick={action} disabled={isDisabled}>
+      {text}
+    </ButtonStyled>
+  );
 };
