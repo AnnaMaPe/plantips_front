25634954a63b21f1b1b16dbfb57d2d816e28ca20
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-19 18:03:40 +0100

    Implement loadTipById function to DetailPage and test it

--- src/pages/DetailPage/DetailPage.test.tsx
+++ src/pages/DetailPage/DetailPage.test.tsx
@@ -1,1 +1,16 @@
-export {};
+import { screen } from "@testing-library/react";
+import { renderRouterWithProviders } from "../../testUtils/renderRouterWithProviders";
+import { DetailPage } from "./DetailPage";
+
+describe("Given the DetailPage page", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a heading with the title 'Maranta lemon'", () => {
+      const expectedTitle = /light/i;
+
+      renderRouterWithProviders({}, <DetailPage />);
+      const heading = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
