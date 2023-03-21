e2eb0fababf82805f28099cbe3049f58b17191f9
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-21 18:53:40 +0100

    Increment margin in between cards in the TipsList

--- src/components/TipsList/TipsListStyled.tsx
+++ src/components/TipsList/TipsListStyled.tsx
@@ -3,8 +3,7 @@ import styled from "styled-components";
 export const TipsListStyled = styled.ul`
   .list {
     &__item {
-      margin: 20px 0 20px 0;
-      padding: 10px 0;
+      padding: 20px 0;
       border-bottom: 1px solid black;
     }
   }
